2ece7424f8d72e293a696a8d7795c993e240bd00
Radoslav Kiryazov <100125411+radoslavKiryazov@users.noreply.github.com>
2023-11-11 16:43:42 +0100

    workflows 111

--- src/main/java/service/TeamService.java
+++ src/main/java/service/TeamService.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Service
 public class TeamService {
-
+//oooops
     @Autowired
     TeamMemberRepository teamRepository;
     private static final List<TeamMember> team_members = new ArrayList<>();
